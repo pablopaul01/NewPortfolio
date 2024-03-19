d0a5676bc777131717dd5739b5fbfaac1407226b
pablopaul01 <juanpablosalomon@gmail.com>
2024-03-19 00:16:24 -0300

    subiendo hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import mainLayer from '../assets/mainL.png'
+import layer2 from '../assets/l3.svg'
+import { TypeAnimation } from 'react-type-animation'
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='relative h-[92vh] overflow-hidden'>
+      <div className='w-full text-gray-400 flex flex-row justify-start h-full items-center text-3xl sm:text-3xl md:text-5xl lg:text-7xl'>
+        <h1 className='text-white z-30 ml-20 mr-2 md:mr-8'>Juan Pablo Salomón,</h1>
+        <TypeAnimation
+          sequence={[
+            "Frontend Dev",
+            1000,
+            "Backend Dev",
+            1000,
+            "Full Stack Dev",
+            1000,
+          ]}
+          wrapper='span'
+          speed={50}
+          repeat={Infinity}
+          style={{ marginBottom: '0px' }}
+          />
+          <img src={layer2} alt="layer2" className='z-20 hidden md:block absolute bottom-[100px] w-full object-bottom object-cover' />
+          <img src={mainLayer} alt="mainlayer" className='z-10 absolute w-full object-bottom object-cover' />
+      </div>
+    </div>
   )
 }
 
